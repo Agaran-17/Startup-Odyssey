da0e38c2ee862579f927a5f575606d66fad36208
prithi23 <prithiyangaanand266@gmail.com>
2026-07-20 20:56:12 +0530

    feat:add Heading in component

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,10 +1,13 @@
 import HeroBadge from "./HeroBadge";
+import HeroHeading from "./HeroHeading";
 
 export default function Hero() {
   return (
     <section className="relative flex min-h-screen items-center justify-center px-6">
-      <div className="max-w-5xl text-center">
+      <div className="max-w-6xl text-center">
         <HeroBadge />
+
+        <HeroHeading />
       </div>
     </section>
   );
---
+++ src/components/hero/HeroHeading.tsx
@@ -0,0 +1,13 @@
+export default function HeroHeading() {
+  return (
+    <div className="mb-8 text-center">
+      <h1 className="text-6xl font-extrabold leading-tight tracking-tight text-white md:text-8xl">
+        Build Your Startup.
+      </h1>
+
+      <h1 className="mt-2 bg-gradient-to-r from-purple-500 via-blue-500 to-cyan-400 bg-clip-text text-6xl font-extrabold leading-tight tracking-tight text-transparent md:text-8xl">
+        Shape Your Destiny.
+      </h1>
+    </div>
+  );
+}
